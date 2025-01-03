53d069ebcc05d5490a20d94378e2b9c8f6e920d2
Itachi-cri <momosame72@gmail.com>
2025-01-03 23:48:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ function convertToBool(text, fault = 'true') {
 
 //--------------------------Mr Frank--------------------------------------
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "Enter Your SubZero Session ID",
+    SESSION_ID: process.env.SESSION_ID || "rA4kRZJY#JGo6djxaKqqaNQktARdfLCxEslxweMw-DP2ObSyK-fY",
     CAPTION: process.env.CAPTION || "*©  Gᴇɴᴇʀᴀᴛᴇᴅ Bʏ SᴜʙZᴇʀᴏ MD*",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
